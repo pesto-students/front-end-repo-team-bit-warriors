12f1c7cda0b8bb47f5a39784b9cd2e23a5abf213
Mamta Gupta <mamtag962000@gmail.com>
2023-08-01 23:09:20 +0530

    React app created

---
+++ malldekho/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Namaste sabhiko!
+      </header>
+    </div>
+  );
+}
+
+export default App;
