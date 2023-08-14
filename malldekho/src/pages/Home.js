ab21a6f90a28fb425fb5d6e91978e1c94ae7682f
Mamta Gupta <mamtag962000@gmail.com>
2023-08-14 23:20:31 +0530

    Reverted search box changes

--- malldekho/src/pages/Home.js
+++ malldekho/src/pages/Home.js
@@ -1,27 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import SearchBox from '../components/SearchBox';
 import ThumbnailText from '../components/ThumbnailText'
 
 
 const Home = () => {
     const thumnailHeader = "Discover your shopping paradise"
-        const [malls, setMalls] = useState([]); // State to store the fetched malls
 
-    
-        useEffect(() => {
-            // Fetch the data once the component mounts
-            async function fetchData() {
-                try {
-                    const mallsData = await MallService.fetchAllMalls();
-                    setMalls(mallsData);
-                } catch (error) {
-                    console.error("Error fetching malls:", error);
-                }   
-            }
-    
-            fetchData();
-        }, [malls]); // Empty dependency array ensures the effect runs only once
-    
     return (
         <>
             <ThumbnailText text={thumnailHeader}/>
