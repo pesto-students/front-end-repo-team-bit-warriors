4adf9c2a7f821cf668197f016226997f9666fdc4
Mamta Gupta <mamtag962000@gmail.com>
2023-08-15 16:58:39 +0530

    Page not found added

--- malldekho/src/App.js
+++ malldekho/src/App.js
@@ -1,26 +1,6 @@
-// import './App.css';
-// import Header from './components/Header'
-// import Footer from './components/Footer'
-// import SearchBox from './components/SearchBox';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header/>
-
-//       <SearchBox/>
-//       <header className="App-header">
-//         Namaste sabhiko!
-//       </header>
-//       <Footer/>
-//     </div>
-//   );
-// }
-// App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './pages/Home';
 import MallPage from './pages/Mall';
 import StorePage from './pages/Store';
@@ -31,27 +11,29 @@ import LoginPage from './components/Login';
 import RegisterPage from './components/Register';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NotFoundPage from './pages/NotFound';
 
 
 const App = () => {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" component={Home} element={<Home/>} />
-        <Route path="/mall/:mall_id" element={<MallPage/>} />
-        <Route path="/store/:store_id" element={<StorePage/>} />
-        <Route path="/contact" element={<ContactUsPage/>} />
-        <Route path="/about" element={<AboutUsPage/>} />
-        <Route path="/user/:user_id" element={<ProfilePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/signup" element={<RegisterPage/>} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/mall/:mall_id" element={<MallPage />} />
+        <Route path="/store/:store_id" element={<StorePage />} />
+        <Route path="/contact" element={<ContactUsPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/user/:user_id" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Catch-all for unknown routes */}
       </Routes>
-      <Footer/>
-    </Router> 
+      <Footer />
+    </Router>
   );
 };
+
 
 
 export default App;
---
+++ malldekho/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ThumbnailText from "../components/ThumbnailText";
+
+const NotFoundPage = () => {
+
+    const message = "Page not found"
+
+    return(
+        <ThumbnailText text= {message}/>
+    )
+}
+
+export default NotFoundPage;
